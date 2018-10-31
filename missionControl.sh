f056bee9eabba287bdea1b8b4ff5cbaf14a13fce
Caitlin Ostroff <caitlin.ostroff@gmail.com>
2018-10-31 13:33:43 -0400

    I am sure I changed something

--- missionControl.sh
+++ missionControl.sh
@@ -3,6 +3,7 @@ python Broward.py
 python Dade.py
 python Monroe.py
 python Manatee.py
-#python Florida.py
+python Florida.py
 python composite_csvs.py
+python calculations.py
 #bash missionControl.sh
