4aeb48efec8fe921cb1798b1bf6c3a4d6d4f83a2
Caitlin Ostroff <caitlin.ostroff@gmail.com>
2018-10-31 13:33:15 -0400

    I am sure I changed something

--- configuration.py
+++ configuration.py
@@ -1,5 +1,4 @@
 electiondate = "11/6/2018"    # In m/d/yyyy format
 snapshotsdir = "./snapshots/"
-targetdir = "../election-results-parser/rawscrapings/"
 # targetdir is the path to save parsed Elex-formatted results, such as "../election-results-parser/rawscrapings/"
 filename = "summary.zip"
